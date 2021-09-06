a12a6f684fa7e3bac1b202e3e26fb9c60392708e
Rottie <kingtang2180@gmail.com>
2021-09-06 16:54:20 +0800

    handlebars

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const express = require('express')
 const app = express()
 
+
+//Setting template engine as handlebars
+const exphbs = require('express-handlebars');
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  
+  res.render('index')
 })
+
 
 // 設定 port 3000
 app.listen(3000, () => {
