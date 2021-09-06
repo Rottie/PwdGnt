d8e677f9f402d1f624032f1ccecf29d28c1c3249
Rottie <kingtang2180@gmail.com>
2021-09-06 17:33:35 +0800

    Beautify UI

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
-
+const port= 3000
 
 //Setting template engine as handlebars
 const exphbs = require('express-handlebars');
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
+const form = require('./public/javascripts/tools')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
   
-  res.render('index')
+  res.render('index',form)
 })
 
 
 // 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(port, () => {
+ 
+  console.log(`Express app listening on port ${port}.`)
 })
